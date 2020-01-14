b48ba36d4ac46626989f86cc47d63d5b84f30d1c
girivasu <vasu1995giridhar@gmail.com>
2020-01-14 13:02:49 +0530

    First Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CheckFileComponent } from './check-file/check-file.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { ServerComponent } from './server/server.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckFileComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/check-file/check-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-file',
+  templateUrl: './check-file.component.html',
+  styleUrls: ['./check-file.component.css']
+})
+export class CheckFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
