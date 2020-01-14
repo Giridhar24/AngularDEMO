f79af067890e6be17c4252193d283bee7ed07a81
girivasu <vasu1995giridhar@gmail.com>
2020-01-14 12:32:11 +0530

    commit 2

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>warning-alert works!</p>
+  `,
+  styles: [`
+    p{
+      padding: 15px;
+      background-color: mistyrose;
+      border: 1px solid red;
+
+    }`
+  ]
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
